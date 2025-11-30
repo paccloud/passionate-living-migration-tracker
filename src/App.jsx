f3a5b3a7d7fafa26eb684fbdf2b17ce6a85624a7
RyanH <paccloud@gmail.com>
2025-11-29 19:59:08 -0500

    Add Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import MigrationTracker from './MigrationTracker';
 import './App.css';
 
+import { Analytics } from "@vercel/analytics/react"
+
 function App() {
   return (
     <div className="App">
       <MigrationTracker />
+      <Analytics />
     </div>
   );
 }
